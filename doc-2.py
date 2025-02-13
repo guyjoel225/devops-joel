bb9bf94934748e15b5a56647dfac194a8fcb0f75
guyjoel225 <guyjoelkonan50@gmail.com>
2025-02-13 11:41:56 +0000

    feat: correction n'ieme

--- doc-2.py
+++ doc-2.py
@@ -1,25 +1,10 @@
 print("je teste ma connaissance en git ")
-<<<<<<< HEAD
+
 print("bonjour les ami.e.s un peu de debug")
 print("hello , world")
-=======
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-annee = 2025
 
 print(f"{annee} is a good year!!!")
 """c'est commentaire der la page"""
 
 print("ajout de texte pour affirmer les modification")
-<<<<<<< HEAD
->>>>>>> 0075a20 (feat: modification doc)
-=======
-=======
->>>>>>> da0a057 (feat:nouveau fichier pour le test git)
-<<<<<<< HEAD
->>>>>>> 74c95a4 (feat: erreur signalé, du coup correction)
-=======
-=======
->>>>>>> da0a057 (feat:nouveau fichier pour le test git)
->>>>>>> 4894386 (feat: gestion de conflit)
+
