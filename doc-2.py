f9267816ad2e2fa284b3d6fa3836388ceeff8c1a
guyjoel225 <guyjoelkonan50@gmail.com>
2025-02-13 13:05:05 +0000

    feat: new branch

--- doc-2.py
+++ doc-2.py
@@ -1,9 +1,12 @@
 print("je teste ma connaissance en git ")
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("bonjour les ami.e.s un peu de debug")
 print("hello , world")
 =======
+=======
+>>>>>>> 74c95a4 (feat: erreur signalé, du coup correction)
 annee = 2025
 >>>>>>> 0075a20 (feat: modification doc)
 
@@ -12,6 +15,11 @@ print(f"{annee} is a good year!!!")
 
 print("ajout de texte pour affirmer les modification")
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 0075a20 (feat: modification doc)
+=======
+=======
+>>>>>>> da0a057 (feat:nouveau fichier pour le test git)
+>>>>>>> 74c95a4 (feat: erreur signalé, du coup correction)
