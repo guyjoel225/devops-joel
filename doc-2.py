182a41aa6a2d6c95b9a3f97473d43207d8b3fb31
guyjoel225 <guyjoelkonan50@gmail.com>
2025-02-13 11:17:36 +0000

    feat: alerte cpnflit conflit

--- doc-2.py
+++ doc-2.py
@@ -1,3 +1,13 @@
 print("je teste ma connaissance en git ")
+<<<<<<< HEAD
 print("bonjour les ami.e.s un peu de debug")
 print("hello , world")
+=======
+
+annee = 2025
+
+print(f"{annee} is a good year!!!")
+"""c'est commentaire der la page"""
+
+print("ajout de texte pour affirmer les modification")
+>>>>>>> 0075a20 (feat: modification doc)
