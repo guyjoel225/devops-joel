ce59b5ff424d36abde2e99c581b029bc53f649ac
guyjoel225 <guyjoelkonan50@gmail.com>
2025-02-13 11:25:48 +0000

    feat:conflit de fusion

--- doc-2.py
+++ doc-2.py
@@ -4,10 +4,16 @@ print("bonjour les ami.e.s un peu de debug")
 print("hello , world")
 =======
 
+<<<<<<< HEAD
 annee = 2025
 
 print(f"{annee} is a good year!!!")
 """c'est commentaire der la page"""
 
 print("ajout de texte pour affirmer les modification")
+<<<<<<< HEAD
 >>>>>>> 0075a20 (feat: modification doc)
+=======
+=======
+>>>>>>> da0a057 (feat:nouveau fichier pour le test git)
+>>>>>>> 74c95a4 (feat: erreur signalé, du coup correction)
